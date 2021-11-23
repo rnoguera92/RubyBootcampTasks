adc22ee2405ef9a1c5efdbc0c3bd99761f0be771
Roberto Noguera <36283354+rnoguera92@users.noreply.github.com>
2021-11-23 16:22:12 -0600

    fix: added example for activity 4
    -added main.rb to activity 4 to test the gem

---
+++ actividad_04/main.rb
@@ -0,0 +1,11 @@
+require "tournament"
+
+
+Tournament.new([
+  { :home_name => 'A', :home_score => 3, :visitor_name => 'B', :visitor_score => 1 },
+  { :home_name => 'C', :home_score => 0, :visitor_name => 'D', :visitor_score => 0 },
+  { :home_name => 'A', :home_score => 1, :visitor_name => 'C', :visitor_score => 1 },
+  { :home_name => 'B', :home_score => 2, :visitor_name => 'D', :visitor_score => 3 },
+  { :home_name => 'A', :home_score => 2, :visitor_name => 'D', :visitor_score => 1 },
+  { :home_name => 'B', :home_score => 3, :visitor_name => 'C', :visitor_score => 1 }
+]).play!
